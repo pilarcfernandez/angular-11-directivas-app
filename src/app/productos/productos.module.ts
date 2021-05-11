17743dd1ef64faadeaef3f5503168b56a9da3aca
Pilar Fernández <pilar.fernandez@euigs.com>
2021-05-11 16:57:37 +0200

    init

---
+++ src/app/productos/productos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { ProductosRoutingModule } from './productos-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
+    ProductosRoutingModule
+  ]
+})
+export class ProductosModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorMsgDirective } from './directives/error-msg.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
+
+
+
+@NgModule({
+  declarations: [
+    CustomIfDirective,
+    ErrorMsgDirective
+  ], 
+  imports: [
+    CommonModule
+  ], 
+  exports: [
+    CustomIfDirective,
+    ErrorMsgDirective
+  ]
+})
+export class SharedModule { }
